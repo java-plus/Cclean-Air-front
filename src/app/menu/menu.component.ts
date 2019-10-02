329c800b4c71f433d2ef8f8f5750955fad8cd67d
Cecile Peyras <peyrascecile@gmail.com>
2019-10-02 16:31:28 +0200

    menu navigation responsive

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Classe qui gère l'affichage de la barre de navigation
+ */
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,6 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  /**
+   *variable qui permet de gérer l'affichage du menu en mode mobile   *
+   * @memberof MenuComponent
+   */
+  isCollapsed = true;
+
+  /**
+   * constructeur
+   */
   constructor() { }
 
   ngOnInit() {
